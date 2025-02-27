3a9b3b554c22a4ce5d82f1d3231d82eef5636052
Vadym <107150489+vtovkach@users.noreply.github.com>
2025-02-26 20:53:29 -0600

    library for displaying character and reading keyboard input

---
+++ libraries/input_output.asm
@@ -0,0 +1,34 @@
+int 0x20
+
+display_letter:
+    push ax
+    push bx
+    push cx
+    push dx 
+    push si
+    push di 
+    mov  ah, 0x0e    ; Load AH with code for terminal output 
+    mov  bx, 0x000f  ; Load BH page zero and BL color (graphic mode)
+    int  0x10        ; Call BIOS for displaying one letter 
+    pop di 
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret             ; Return to caller
+
+read_keyboard:
+    push bx
+    push cx
+    push dx 
+    push si
+    push di 
+    mov  ah, 0x00   ; Load Ah with code for keyboard read 
+    int  0x16       ; Call the BIOS for reading keyboard
+    pop di
+    pop si 
+    pop dx 
+    pop cx 
+    pop bx 
+    ret             ; Return to caller 
