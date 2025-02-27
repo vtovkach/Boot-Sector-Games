ee2af6e604b26e47e1d543ef37101594d22d68ed
Vadim0036 <vadim003600@gmail.com>
2025-02-26 21:37:17 -0600

    ASCII Char

--- MS-DOS Examples/Arithmetic/program_1.asm
+++ MS-DOS Examples/Arithmetic/program_1.asm
@@ -1,1 +1,37 @@
+; Addition 
 
+org 0x0100 
+
+start: 
+    mov al, 0x04
+    add al, 0x03
+    ;
+    add al, 0x30
+    call display_letter
+
+    ; Print new line char
+    mov al, 0x0d
+    call display_letter
+    ;
+    mov al, 0x0a           
+    call display_letter
+
+    int 0x20  ; Terminate Process 
+
+display_letter:
+    push ax
+    push bx
+    push cx
+    push dx 
+    push si
+    push di 
+    mov  ah, 0x0e    ; Load AH with code for terminal output 
+    mov  bx, 0x000f  ; Load BH page zero and BL color (graphic mode)
+    int  0x10        ; Call BIOS for displaying one letter 
+    pop di 
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret             ; Return to caller
