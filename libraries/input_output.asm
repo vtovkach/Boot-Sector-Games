f6b8470f12d23946ccc21850d27f671ba33ef58d
Vadim0036 <vadim003600@gmail.com>
2025-03-06 20:13:17 -0600

    Modify Library

--- libraries/input_output.asm
+++ libraries/input_output.asm
@@ -32,3 +32,17 @@ read_keyboard:
     pop cx 
     pop bx 
     ret             ; Return to caller 
+
+display_number:
+    mov dx, 0
+    mov cx, 10
+    div cx    ; divide ax by cx
+    push dx 
+    cmp ax, 0 
+    je display_number_1
+    call display_number
+display_number_1:
+    pop ax
+    add al, '0'
+    call display_letter
+    ret  
