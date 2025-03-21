99d52dd0c4878f5562570436946a987e523fe871
Vadim0036 <vadim003600@gmail.com>
2025-03-20 21:53:42 -0600

    Updated Matrix Boot Prog; still issue

--- boot-sector-programs/matrix/matrix.asm
+++ boot-sector-programs/matrix/matrix.asm
@@ -1,33 +1,71 @@
 
 org 0x7c00
 
-COLOR equ 0x0002
+COLOR        equ 0x0002
+BASE_CHAR    equ 0x41 ; base letter 'A'
+BASE_VIDEO   equ 0xb800
 
 start:
+    ;Boot Sector Initialization 
     mov ax, cs
     mov ds, ax 
 
-    mov ax, 0x0003
-    int 0x10
-
-    mov ax, 0xb800
+    mov ax, BASE_VIDEO
     mov es, ax
 
-    xor di, di 
-    mov si, msg
-print_msg:
-    mov al, [si]
-    cmp al, 0
-    je inf_loop
+    xor di, di  
+    xor dx, dx 
+
+    mov dl, 0 ; letter offset
+    mov bx, 0 ; row counter 
+
+test_module: 
+    mov al, BASE_CHAR
     mov ah, COLOR
-    stosw  
-    inc si
-    jmp print_msg
+    call print_line
+    jmp end_loop
 
-inf_loop:
-    jmp inf_loop
+; ISSUE HERE is that print_line subroutine never returns 
 
-msg db "Hello World", 0
+%if 0
+print_screen:
+    ;Main Loop
+    cmp dl, 26
+    jl endif1 
+if_1:
+    mov dl, 0
+endif1:
+    mov al, BASE_CHAR
+    add al, dl
+    mov ah, COLOR
+    call print_line
+
+    inc bx ; increment row counter 
+    inc dl ; letter offset 
+    cmp bx, 25
+    jl endif2
+if_2:
+    mov bx, 0 
+    mov ax, BASE_VIDEO
+    mov es, ax
+    xor di, di 
+endif2:
+    jmp print_screen
+%endif
+
+end_loop:
+    jmp end_loop
+
+
+print_line: 
+    ; TODO
+    ; subroutine to print a line
+    ; subroutine accepts already prepared ax register
+    mov cx, 80 ; columnt counter 
+    stosw
+    ; iterate until cx is 0
+    loop print_line
+    ret 
 
 times 510-($-$$) db 0
-dw 0x55aa
+db 0x55, 0xaa 
